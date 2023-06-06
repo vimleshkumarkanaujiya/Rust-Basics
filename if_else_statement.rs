770bb1ca12609c88d348372cf44782f7404ad322
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-06-07 02:48:36 +0530

    Create if_else_statement.rs

---
+++ if_else_statement.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let num = 10;
+
+    if num > 5 {
+        println!("The number is greater than 5");
+    } else {
+        println!("The number is less than or equal to 5");
+    }
+}
+
+
+/* Output
+The number is greater than 5
+*/
