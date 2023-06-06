8d97f005af530d9b6fc7705ea3d1731ff06e435c
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-06-07 02:52:01 +0530

    Create if_else_if_statement.rs

---
+++ if_else_if_statement.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let num = 10;
+
+    if num > 10 {
+        println!("The number is greater than 10");
+    } else if num < 10 {
+        println!("The number is less than 10");
+    } else {
+        println!("The number is equal to 10");
+    }
+}
+
+
+/* Output
+The number is equal to 10
+*/
