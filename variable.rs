a861f375b1780b2bae55cdf473efaf52524a26b8
Vimlesh Kumar Kanaujiya <kanaujiyavimleshkumar@gmail.com>
2023-06-07 02:32:51 +0530

    Create variable.rs

---
+++ variable.rs
@@ -0,0 +1,27 @@
+fn main() {
+    // Integer variables
+    let integer_num: i32 = 10;
+    let unsigned_integer: u32 = 20;
+
+    // Floating-point variables
+    let float_num: f32 = 3.14;
+    let double_num: f64 = 3.14159265359;
+
+    // Boolean variable
+    let is_true: bool = true;
+
+    // Character variable
+    let char_value: char = 'A';
+
+    // String variable
+    let string_value: &str = "Hello, Rust!";
+
+    // Display variable values
+    println!("Integer: {}", integer_num);
+    println!("Unsigned Integer: {}", unsigned_integer);
+    println!("Float: {}", float_num);
+    println!("Double: {}", double_num);
+    println!("Boolean: {}", is_true);
+    println!("Character: {}", char_value);
+    println!("String: {}", string_value);
+}
